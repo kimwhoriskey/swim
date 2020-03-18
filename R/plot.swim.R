e7c16b2255366e3ea316a13a9fede7f6e6795620
kimwhoriskey <kwhoriskey@dal.ca>
2020-03-18 16:46:31 -0300

    as shared with benia nowak on 27012017

--- R/plot.swim.R
+++ R/plot.swim.R
@@ -5,8 +5,11 @@ plot.swim <- function(x,...){
   data = x$regData
   states = x$states
   
-  plot(data[states==1,]$lat~data[states==1,]$lon, type="o", pch=20, cex=0.7, col="grey", 
+  plot(data$lat~data$lon, type="o", pch=20, cex=0.7, 
+       col=ifelse(states==2, "blue", "grey"), 
        xlab="Longitude", ylab="Latitude")
-  points(data[states==2,]$lat~data[states==2,]$lon, type="p", pch=20, cex=0.7, col="blue")
+#   plot(data[states==1,]$lat~data[states==1,]$lon, type="o", pch=20, cex=0.7, col="grey", 
+#        xlab="Longitude", ylab="Latitude")
+#   points(data[states==2,]$lat~data[states==2,]$lon, type="p", pch=20, cex=0.7, col="blue")
   
 }
